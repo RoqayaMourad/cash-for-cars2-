2fdb8bd0fe69a945913ba3515834400e65ff6a0d
roqaya299 <elephantsong29@example.com>
2021-01-11 03:24:52 +0200

    sticky header and transition on desktop

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     $(window).scroll(function () { 
       console.log($(window).scrollTop());
   
-      if ($(window).scrollTop() > 550) {
+      if ($(window).scrollTop() > 48) {
         $('.header-container').addClass('header-scrolled');
       }
   
-      if ($(window).scrollTop() < 551) {
+      if ($(window).scrollTop() < 49) {
         $('.header-container').removeClass('header-scrolled');
       }
     });
